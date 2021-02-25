4a314e41c2fc37caf6a08ed20268df90d6aab657
Maria Rushkova <maryrushkova@gmail.com>
2021-02-25 12:19:56 +0100

    added bookmark function

--- src/js/controller.js
+++ src/js/controller.js
@@ -56,9 +56,16 @@ const controlServings = function (numServings) {
   recipeView.update(model.state.recipe);
 };
 
+const controlAddBookmark = function () {
+  if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
+  else model.deleteBookmark(model.state.recipe.id);
+  recipeView.update(model.state.recipe);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   recipeView.addHandlerUpdateServings(controlServings);
+  recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchRusults);
   paginationView.addHandlerClick(controlPagination);
 };
